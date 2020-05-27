c744ed6a9d432f8958e32b660c1776d154495908
Rajan Balana <rajan.balana@oyorooms.com>
2020-05-27 10:17:14 +0530

    Initial Commit

---
+++ RBChatAppTests/RBChatAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  RBChatAppTests.swift
+//  RBChatAppTests
+//
+//  Created by Rajan Balana on 27/05/20.
+//  Copyright © 2020 Rajan Balana. All rights reserved.
+//
+
+import XCTest
+@testable import RBChatApp
+
+class RBChatAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
